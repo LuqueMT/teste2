b51bda999d056d30f6753576b242df7f48e1c33f
Luís Henrique M. Troquato <luhemt@outlook.com>
2025-03-26 17:36:54 -0300

    caminhos modificados da pasta img

--- src/app/certificados/page.tsx
+++ src/app/certificados/page.tsx
@@ -3,10 +3,10 @@ import Navbar from '../../components/Navbar/Navbar.jsx'
 import Footer from '@/components/Footer/Footer.jsx'
 import img1 from '../../../public/assests/pdf/certificado1.png'
 import img2 from '../../../public/assests/pdf/certificado2.png'
-import img3 from '../../../public/assests/pdf/certificado3.png'
-import img4 from '../../../public/assests/pdf/certificado4.png'
-import img5 from '../../../public/assests/pdf/certificado5.png'
-import img6 from '../../../public/assests/pdf/certificado6.png'
+import img3 from '../../../public/assests/pdf/Certificado3.png'
+import img4 from '../../../public/assests/pdf/Certificado4.png'
+import img5 from '../../../public/assests/pdf/Certificado5.png'
+import img6 from '../../../public/assests/pdf/Certificado6.png'
 import "./style.css";
 import Image from 'next/image';
 export default function Cetificados() {
